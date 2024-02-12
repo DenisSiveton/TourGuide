0d71540070432d9cfca7b7c64cc9b564c0d29205
Denis Siveton <denis.siveton@hotmail.com>
2024-02-12 12:18:38 +0100

    update test "getTripDeals()" from TestTourGuideService.java. The method used returned a list with a length of 5 instead of 10.

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -111,6 +111,7 @@ public class TestTourGuideService {
 		assertEquals(5, attractions.size());
 	}
 
+	@Test
 	public void getTripDeals() {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
@@ -123,7 +124,7 @@ public class TestTourGuideService {
 
 		tourGuideService.tracker.stopTracking();
 
-		assertEquals(10, providers.size());
+		assertEquals(5, providers.size());
 	}
 
 }
